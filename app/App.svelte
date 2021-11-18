f1a076bd989ac348baf94ef4206ce22bdae3fed8
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-18 19:34:46 +0530

    trying to play audio , not working

--- app/App.svelte
+++ app/App.svelte
@@ -15,14 +15,42 @@
             </formattedString>
         </label>
         <button class="counter" text="-" on:tap="{onButtonTap}" />
+        <label>
+            Remind allah atleast 30 minutes once
+        </label>
+        <label>
+            Play audio
+        </label>
+        <button class="play_audio_btn" text="Play Audio" 
+            on:tap="{playAudioBtn}" 
+        />
     </stackLayout>
 </page>
 
 <script lang="typescript">
+    import { TNSPlayer } from 'nativescript-audio-player';
     let message: string = "Allah Count: ";
     let startCount = 1000;
     let count = 1000;
 
+    let player = new TNSPlayer();
+
+    function playAudioBtn() {
+        console.log("inside play audio button");
+        player.initFromFile({
+            audioFile: '~/audio/asma_ul_husna.mp3', // ~ = app directory
+            loop: false,
+            completeCallback: () =>{console.log("completed playing")},
+            errorCallback: () => {console.log("error in playing")}
+        })
+        .then(() => {
+            player.getAudioTrackDuration().then(duration => {
+            // iOS: duration is in seconds
+            // Android: duration is in milliseconds
+            console.log(`song duration:`, duration);
+            });
+        });
+    }
     function onButtonTap() {
         console.log("inside onButtonTap");
         if(count>0)
@@ -32,6 +60,8 @@
         console.log("inside setSTartCount ", startCount);
         count = startCount;
     }
+
+    // setInterval(function(){ alert("Hello"); }, 3000);
 </script>
 
 <style>
